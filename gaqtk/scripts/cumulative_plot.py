33e33e31771a4fe0e2f7295d11f0f07def351812
Per Unneberg <per.unneberg@scilifelab.se>
2015-03-27 16:40:35 +0100

    * gaqtk/gaqtk/bokeh/scatter.py: add cumulative_contig * gaqtk/scripts/cumulative_plot.py: update example script

--- gaqtk/scripts/cumulative_plot.py
+++ gaqtk/scripts/cumulative_plot.py
@@ -5,14 +5,15 @@ import os
 import io
 import jinja2
 import textwrap
-from gaqtk.bokeh.scatter import scatter
+from gaqtk.bokeh.scatter import scatter, cumulative_contigs
 from gaqtk.bokeh.publish import static_html
 
 def main():
-    x = list(range(5))
-    y = list(range(5))
+    x = list(range(1, 5))
+    y = list(range(1, 5))
+    z = ["c1", "c2", "c3", "c4", "c5"]
     p1 = scatter(x=x, y=y, title="Plot 1")
-    p2 = scatter(x=x, y=y, title="Plot 2")
+    p2 = cumulative_contigs(zip(z, y))
     kw = {'p1' : p1, 'p2' : p2}
     html = static_html(**kw)
     with open("scatter.html", "w") as fh:
