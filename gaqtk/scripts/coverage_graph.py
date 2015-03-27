6be1003fe8e868b173e0e9bc640bf05caa055fdd
Johan Viklund <johan.viklund@bils.se>
2015-03-27 11:57:17 +0100

    [plotting] coverage_graph binning
    * Show Min and Max and mean within each bin.
    * Scaffolds are highlighted with 4 different shades of brown.

--- gaqtk/scripts/coverage_graph.py
+++ gaqtk/scripts/coverage_graph.py
@@ -7,10 +7,7 @@ import pysam
 import numpy
 
 from bokeh.plotting import figure, output_file, save
-#from matplotlib import pyplot
 
-#import matplotlib.pyplot as plt
-#import mpld3
 
 class CoveragePlot(object):
     
@@ -18,6 +15,7 @@ class CoveragePlot(object):
         self.assembly = assembly
         output_file("scatter.html")
         self.p1 = figure(plot_width=700, plot_height=300)
+        self.sections = []
         
     def plot(self, name, length, cov, offset):
         print "%s %s %s" % (name, length, len(cov))
@@ -28,15 +26,38 @@ class CoveragePlot(object):
             nbins = 10
             local_bin_width = length / (nbins - 1)
 
-        cov_bins = [0] * nbins
+        cov_bins = {
+            "min":  [float('Inf')] * nbins,
+            "max":  [0.0] * nbins,
+            "mean": [0.0] * nbins,
+        }
         for i, x in enumerate(cov):
-            cov_bins[ i/local_bin_width ] += x
+            bini = i / local_bin_width
+            if cov_bins['min'][bini] > x:
+                cov_bins['min'][bini] = x
+            elif cov_bins['max'][bini] < x:
+                cov_bins['max'][bini] = x
+            cov_bins['mean'][bini] += x
 
-        cov_bins = [ x/local_bin_width for x in cov_bins ]
-        
-        self.p1.line(range(offset/local_bin_width,offset/local_bin_width+len(cov_bins)), cov_bins, size=12, color="red", alpha=0.5)
+        l = local_bin_width * 1.0
+        for i, x in enumerate(cov_bins['mean']):
+            #print cov_bins[i][2]
+            cov_bins['mean'][i] = x*1.0/l
+        cov_bins['x_vals'] = [x*local_bin_width for x in range(offset/local_bin_width, offset/local_bin_width+len(cov_bins['mean']))]
+        cov_bins['length'] = length
+        self.sections.append(cov_bins)
 
     def save(self):
+        mx = max( [ max(x['max']) for x in self.sections ] )
+        colors = ['#D8A031', '#D37600', '#C16A00', '#A84600']
+        for i, cov_bins in enumerate(self.sections):
+            x_vals = cov_bins['x_vals']
+            self.p1.rect(x_vals[0] + cov_bins['length']/2, 0+mx/2, cov_bins['length'], mx, color=colors[i%4], alpha=1)
+
+            self.p1.line(x_vals, cov_bins['min'], size=12, color="green", alpha=1)
+            self.p1.line(x_vals, cov_bins['max'], size=12, color="red", alpha=1)
+            self.p1.line(x_vals, cov_bins['mean'], size=12, color="blue", alpha=1)
+
         save(self.p1)
     
     def run(self):
@@ -51,7 +72,7 @@ class CoveragePlot(object):
         for i in samfile.header['SQ']:
             header.append( (i['SN'], i['LN']) )
             tot_length += i['LN']
-        self.bin_width = tot_length/10000
+        self.bin_width = tot_length/1000
 
         length_sofar = 0
         
